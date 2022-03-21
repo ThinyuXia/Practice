81802f9a3ef65a2d315c0b8fc58f6005eca02f38
= <2316683971@qq.com>
2022-03-21 15:38:50 +0800

    Cookie(小甜饼)用来保存用户资料以及登录状态

---
+++ 慕课网java学习代码/web基础/servlet/src/main/java/com/xiaxinyu/servlet/cookie/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.xiaxinyu.servlet.cookie;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/cookies/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("用户登录成功");
+		Cookie cookie = new Cookie("user","admin");
+		response.addCookie(cookie); 
+		response.getWriter().println("success");
+	}
+
+}
