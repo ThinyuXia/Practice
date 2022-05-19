ce09e8f9cd9ce35c7f6afd8c8241c2e147a0b436
= <2316683971@qq.com>
2022-05-19 23:45:06 +0800

    查看ioc容器内对象

--- java学习代码/SSM/Spring基础/ioc/src/main/java/com/xiaxinyu/spring/ioc/SpringApplication.java
+++ java学习代码/SSM/Spring基础/ioc/src/main/java/com/xiaxinyu/spring/ioc/SpringApplication.java
@@ -31,11 +31,27 @@ public class SpringApplication {
         Apple apple = context.getBean("sweetApple",Apple.class);
         System.out.println(apple.getTitle());
 
+        //1.当有多个类型相同的匿名bean标签时，IoC容器中他们的标识符为类名全称#{数字(从0开始)}
+        //2.使用context.getBean("类全称")方法获取的是第一个bean标签对象
+        //3.想获取后续的对象需要加上#以及编号
         Apple apple1 = context.getBean("com.xiaxinyu.spring.ioc.entity.Apple",Apple.class);
         System.out.println(apple1.getTitle());
 
         Company company = context.getBean("company",Company.class);
         System.out.println(company);
+
+
+        System.out.println("========================");
+
+        //获取容器内所有beanId的数组
+        String[] beanNames = context.getBeanDefinitionNames();
+        for(String beanName : beanNames){
+            System.out.println(beanName);
+            System.out.println("类型：" + context.getBean(beanName).getClass().getName());
+            System.out.println("类型：" + context.getBean(beanName).toString());
+        }
+
+
 
     }
 }
