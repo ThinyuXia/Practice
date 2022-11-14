7e4a5d954d51111233340ddf39288a002be951f2
夏新宇 <2316683971@qq.com>
2022-11-14 21:15:21 +0800

    基本数据类型优化 && 并行流

--- java学习代码/lambda/src/main/java/com/xiaxinyu/stream/StreamDemo.java
+++ java学习代码/lambda/src/main/java/com/xiaxinyu/stream/StreamDemo.java
@@ -1,9 +1,7 @@
 package com.xiaxinyu.stream;
 
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -66,10 +64,72 @@ public class StreamDemo {
 //        authorStream.map(author -> author.getName())
 //                .forEach(sb::append);
 
+//        authors.stream()
+//                .map(Author::getName)
+//                .map(StringBuilder::new)
+//                .map(sb -> sb.append("-三更").toString())
+//                .forEach(System.out::println);
+
+
+//        authors.stream()
+//                .map(author -> author.getAge())
+//                .map(age -> age + 10)
+//                .filter(age->age>18)
+//                .map(age->age+2)
+//                .forEach(System.out::println);
+
+
+//        authors.stream()
+//                .mapToInt(author -> author.getAge())
+//                .map(age -> age + 10)
+//                .filter(age->age>18)
+//                .map(age->age+2)
+//                .forEach(System.out::println);
+        test28();
+    }
+
+    private static void test28() {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//        Integer integer = stream.filter(num -> num > 5)
+//                .reduce(Integer::sum).get();
+//
+//        System.out.println(integer);
+
+//        Integer sum = stream.parallel()
+//                .peek(new Consumer<Integer>() {
+//                    @Override
+//                    public void accept(Integer num) {
+//                        System.out.println(num + Thread.currentThread().getName());
+//                    }
+//                })
+//                .filter(num -> num > 5)
+//                .reduce(Integer::sum)
+//                .get();
+
+        List<Author> authors = getAuthors();
+        authors.parallelStream()
+                .map(author -> author.getAge())
+                .map(age -> age + 10)
+                .filter(age->age>18)
+                .map(age->age+2)
+                .forEach(System.out::println);
+    }
+
+    private static void test27() {
+
+        List<Author> authors = getAuthors();
         authors.stream()
-                .map(Author::getName)
-                .map(StringBuilder::new)
-                .map(sb->sb.append("-三更").toString())
+                .map(author -> author.getAge())
+                .map(age -> age + 10)
+                .filter(age -> age > 18)
+                .map(age -> age + 2)
+                .forEach(System.out::println);
+
+        authors.stream()
+                .mapToInt(author -> author.getAge())
+                .map(age -> age + 10)
+                .filter(age -> age > 18)
+                .map(age -> age + 2)
                 .forEach(System.out::println);
     }
 
@@ -146,7 +206,7 @@ public class StreamDemo {
         List<Author> authors = getAuthors();
         Integer sum = authors.stream()
                 .map(author -> author.getAge())
-                .reduce(0,(a,b) -> a + b);
+                .reduce(0, (a, b) -> a + b);
         System.out.println(sum);
     }
 
@@ -302,7 +362,7 @@ public class StreamDemo {
         List<Author> authors = getAuthors();
         authors.stream()
                 .distinct()
-                .sorted((a,b) -> b.getAge() - a.getAge())
+                .sorted((a, b) -> b.getAge() - a.getAge())
                 .skip(1)
                 .forEach(author -> System.out.println(author.getName()));
 
@@ -314,7 +374,7 @@ public class StreamDemo {
         List<Author> authors = getAuthors();
         authors.stream()
                 .distinct()
-                .sorted((a,b) -> b.getAge() - a.getAge())
+                .sorted((a, b) -> b.getAge() - a.getAge())
                 .limit(2)
                 .forEach(author -> System.out.println(author.getName()));
 
@@ -329,7 +389,7 @@ public class StreamDemo {
 //                .forEach(author -> System.out.println(author.getAge()));
         authors.stream()
                 .distinct()
-                .sorted((a,b) -> a.getAge() - b.getAge())
+                .sorted((a, b) -> a.getAge() - b.getAge())
                 .forEach(author -> System.out.println(author.getAge()));
     }
 
@@ -367,10 +427,10 @@ public class StreamDemo {
 
     //双列集合先转化成单列集合再使用stream()方法
     private static void test03() {
-        Map<String,Integer> map = new HashMap<>();
-        map.put("蜡笔小新",19);
-        map.put("黑子",17);
-        map.put("日向翔阳",16);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("蜡笔小新", 19);
+        map.put("黑子", 17);
+        map.put("日向翔阳", 16);
 
         Stream<Map.Entry<String, Integer>> stream = map.entrySet().stream();
         stream.filter(entry -> entry.getValue() > 16)
@@ -380,7 +440,7 @@ public class StreamDemo {
 
     //数组创建流对象使用Arrays.stream()方法或者Stream.of()方法
     private static void test02() {
-        Integer[] arr = {1,2,3,4,5};
+        Integer[] arr = {1, 2, 3, 4, 5};
 //        Stream<Integer> stream = Arrays.stream(arr);
         Stream<Integer> stream = Stream.of(arr);
         stream.distinct()
@@ -392,40 +452,40 @@ public class StreamDemo {
     //集合类创建流对象使用 stream() 方法
     private static void test01(List<Author> authors) {
         authors.stream() //把集合转换成流
-                .distinct() 
+                .distinct()
                 .filter(author -> author.getAge() < 18)
                 .forEach(author -> System.out.println(author.getName()));
     }
 
     private static List<Author> getAuthors() {
         //数据初始化
-        Author author = new Author(1L,"蒙多",33,"一个从菜刀中明悟哲理的祖安人",null);
-        Author author2 = new Author(2L,"亚拉索",15,"狂风也追逐不上他的思考速度",null);
-        Author author3 = new Author(3L,"易",14,"是这个世界在限制他的思维",null);
-        Author author4 = new Author(3L,"易",14,"是这个世界在限制他的思维",null);
+        Author author = new Author(1L, "蒙多", 33, "一个从菜刀中明悟哲理的祖安人", null);
+        Author author2 = new Author(2L, "亚拉索", 15, "狂风也追逐不上他的思考速度", null);
+        Author author3 = new Author(3L, "易", 14, "是这个世界在限制他的思维", null);
+        Author author4 = new Author(3L, "易", 14, "是这个世界在限制他的思维", null);
 
         //书籍列表
         List<Book> books1 = new ArrayList<>();
         List<Book> books2 = new ArrayList<>();
         List<Book> books3 = new ArrayList<>();
 
-        books1.add(new Book(1L,"刀的两侧是光明与黑暗","哲学,爱情",88,"用一把刀划分了爱恨"));
-        books1.add(new Book(2L,"一个人不能死在同一把刀下","个人成长,爱情",99,"讲述如何从失败中明悟真理"));
+        books1.add(new Book(1L, "刀的两侧是光明与黑暗", "哲学,爱情", 88, "用一把刀划分了爱恨"));
+        books1.add(new Book(2L, "一个人不能死在同一把刀下", "个人成长,爱情", 99, "讲述如何从失败中明悟真理"));
 
-        books2.add(new Book(3L,"那风吹不到的地方","哲学",85,"带你用思维去领略世界的尽头"));
-        books2.add(new Book(3L,"那风吹不到的地方","哲学",85,"带你用思维去领略世界的尽头"));
-        books2.add(new Book(4L,"吹或不吹","爱情,个人传记",56,"一个哲学家的恋爱观注定很难把他所在的时代理解"));
+        books2.add(new Book(3L, "那风吹不到的地方", "哲学", 85, "带你用思维去领略世界的尽头"));
+        books2.add(new Book(3L, "那风吹不到的地方", "哲学", 85, "带你用思维去领略世界的尽头"));
+        books2.add(new Book(4L, "吹或不吹", "爱情,个人传记", 56, "一个哲学家的恋爱观注定很难把他所在的时代理解"));
 
-        books3.add(new Book(5L,"你的剑就是我的剑","爱情",56,"无法想象一个武者能对他的伴侣这么的宽容"));
-        books3.add(new Book(6L,"风与剑","个人传记",100,"两个哲学家灵魂和肉体的碰撞会激起怎么样的火花呢？"));
-        books3.add(new Book(6L,"风与剑","个人传记",100,"两个哲学家灵魂和肉体的碰撞会激起怎么样的火花呢？"));
+        books3.add(new Book(5L, "你的剑就是我的剑", "爱情", 56, "无法想象一个武者能对他的伴侣这么的宽容"));
+        books3.add(new Book(6L, "风与剑", "个人传记", 100, "两个哲学家灵魂和肉体的碰撞会激起怎么样的火花呢？"));
+        books3.add(new Book(6L, "风与剑", "个人传记", 100, "两个哲学家灵魂和肉体的碰撞会激起怎么样的火花呢？"));
 
         author.setBooks(books1);
         author2.setBooks(books2);
         author3.setBooks(books3);
         author4.setBooks(books3);
 
-        List<Author> authorList = new ArrayList<>(Arrays.asList(author,author2,author3,author4));
+        List<Author> authorList = new ArrayList<>(Arrays.asList(author, author2, author3, author4));
         return authorList;
     }
 }
